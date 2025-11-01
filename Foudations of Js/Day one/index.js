654bdb6abeea18b23259d0be38f29da225fdff37
Ghanashyam Budhathoki <sdev1252492@gmail.com>
2025-11-01 13:45:18 +0545

    Add files via upload

---
+++ Foudations of Js/Day one/index.js
@@ -0,0 +1,26 @@
+/* Hello everyone in this course we learn js from begineer to advanced level. 
+So let's get started. 
+First step make folder and open folder in code editor like vscode, notepad++ i 
+suggest you too use vscode. 
+And after that give any name of file but anyhow you need to add .js it 
+is compulsory one. Without this your execution is not sucessfull 
+so lets see your first code with explanation. 
+*/
+console.log("Hello Strangers how are you ? "); 
+
+/* Explanation : 
+console.log means push your text to display in the screen.   
+*/
+/* This is optional but you really need to know : 
+ What is JavaScript
+
+JavaScriptis a programming language developed by Brendan Eich.  It's used to create **interactive web pages**, mobile applications, and server-side applications.
+
+#Uses of JavaScript:
+
+- It powers dynamic content on web pages, creating interactive experiences.
+- It enables **frontend** development through frameworks like React, Angular, and Vue.
+- It works on the **backend** with Node.js, allowing full-stack JavaScript development.
+*/
+/* In Day two we would learn data types and variable bye for now. 
+*/
